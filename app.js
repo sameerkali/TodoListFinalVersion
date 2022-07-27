406028685b449bb6d6f8c4a1633195dd6b761946
Sameer Faridi <s.faridi007@gmail.com>
2022-07-27 22:10:34 +0530

    2.1

--- app.js
+++ app.js
@@ -143,6 +143,5 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+app.listen(process.env.PORT || 3000, function(){})
+
